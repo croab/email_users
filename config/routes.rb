8bc76e893b183893ba35669cf9b60088e9e36b20
Benjamin Croad <benjamincroadapple@gmail.com>
2022-12-12 12:00:09 +0000

    Sessions controller to login and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post "/login", to: "sessions#login"
+  post "/signup", to: "sessions#signup"
 end
